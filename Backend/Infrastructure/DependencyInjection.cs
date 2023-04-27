2c033af5c056f795c34faec7d3f64850fce39908
Vasilescu Alexandru <vasilescu.alexandru.a3m@student.ucv.ro>
2023-04-27 11:08:28 +0300

    push test

--- Backend/Infrastructure/DependencyInjection.cs
+++ Backend/Infrastructure/DependencyInjection.cs
@@ -6,7 +6,6 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services)
     {
-        int i;
         return services;
     }
 }
