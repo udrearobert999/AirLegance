3640083fcd832699590f3cf60c020a84b6f43188
Robert Udrea <udrearobert999@gmail.com>
2023-04-27 11:12:25 +0300

    Solved Routing in Frontend

--- Frontend/src/Pages/Login/Login.jsx
+++ Frontend/src/Pages/Login/Login.jsx
@@ -13,7 +13,7 @@ import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-import * as routes from "../../routes.js";
+import * as routes from "../../Routes";
 
 function Copyright(props) {
   return (
---
+++ Frontend/src/Routes.js
@@ -0,0 +1,5 @@
+export const REGISTER_ROUTE = '/register';
+export const LOGIN_ROUTE = '/login';
+export const CONTACT_ROUTE = '/contact';
+export const ABOUT_ROUTE = '/about';
+export const HOME_ROUTE = '/';
