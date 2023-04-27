12a9eae8bf4f83d1b1756fe964cc2558fc8dece3
DESKTOP-E3FJNL7\User <vasilescu.alexandru.a3m@student.ucvro>
2023-04-27 11:06:07 +0300

    test push

--- Backend/Infrastructure/DependencyInjection.cs
+++ Backend/Infrastructure/DependencyInjection.cs
@@ -6,6 +6,7 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services)
     {
+        int i;
         return services;
     }
 }
