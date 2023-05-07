19b27549edb0c2e1156cd512c6a191f89097610c
Robert Udrea <udrearobert999@gmail.com>
2023-05-07 17:10:44 +0300

    ARL-19 Secure User Login: - Added funct to override default mui style

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,16 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { StyledEngineProvider } from '@mui/material/styles';
 
-import App from "./App";
+import './index.css';
 
-import { BrowserRouter } from "react-router-dom";
+import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+import { BrowserRouter } from 'react-router-dom';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+    <StyledEngineProvider injectFirst>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </StyledEngineProvider>
+  </React.StrictMode>
 );
