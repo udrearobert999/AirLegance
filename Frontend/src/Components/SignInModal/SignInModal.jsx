f0dffc34e4fc921c98929aa770cd2a89775b0d93
Robert Udrea <udrearobert999@gmail.com>
2023-05-09 22:43:14 +0300

    ARL-19 Secure User Login: - Refactored register page

--- Frontend/src/Components/SignInModal/SignInModal.jsx
+++ Frontend/src/Components/SignInModal/SignInModal.jsx
@@ -1,24 +1,29 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
+import React from 'react';
+
+import {
+  Avatar,
+  Button,
+  TextField,
+  FormControlLabel,
+  Checkbox,
+  Grid,
+  Box,
+  Link,
+  Typography,
+  Container,
+  Backdrop,
+  Modal,
+  Fade,
+} from '@mui/material';
+
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import Backdrop from '@mui/material/Backdrop';
-import Modal from '@mui/material/Modal';
-import Fade from '@mui/material/Fade';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+import Style from './SignInModal.module.css';
 
 function Copyright(props) {
   return (
     <Typography
+      className={Style.copyrightTypography}
       variant='body2'
       color='text.secondary'
       align='center'
@@ -33,20 +38,6 @@ function Copyright(props) {
     </Typography>
   );
 }
-
-const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 600,
-  borderRadius: 10,
-  bgcolor: 'background.paper',
-  boxShadow: 24,
-  p: 4,
-};
-
-const theme = createTheme();
 
 export default function SignInModal({ open, handleClose }) {
   const handleSubmit = (event) => {
@@ -70,79 +61,70 @@ export default function SignInModal({ open, handleClose }) {
       }}
     >
       <Fade in={open}>
-        <Box sx={style}>
-          <ThemeProvider theme={theme}>
-            <Container component='main' maxWidth='xs'>
-              <CssBaseline />
+        <Box className={Style.modalBox}>
+          <Container component='main' maxWidth='xs'>
+            <Box className={Style.formContainer}>
+              <Avatar className={Style.avatar}>
+                <LockOutlinedIcon />
+              </Avatar>
+              <Typography component='h1' variant='h5'>
+                Sign in
+              </Typography>
               <Box
-                sx={{
-                  marginTop: 8,
-                  display: 'flex',
-                  flexDirection: 'column',
-                  alignItems: 'center',
-                }}
+                component='form'
+                onSubmit={handleSubmit}
+                noValidate
+                className={Style.formMarginTop}
               >
-                <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                  <LockOutlinedIcon />
-                </Avatar>
-                <Typography component='h1' variant='h5'>
-                  Sign in
-                </Typography>
-                <Box
-                  component='form'
-                  onSubmit={handleSubmit}
-                  noValidate
-                  sx={{ mt: 1 }}
+                <TextField
+                  margin='normal'
+                  required
+                  fullWidth
+                  id='email'
+                  label='Email Address'
+                  name='email'
+                  autoComplete='email'
+                  autoFocus
+                />
+                <TextField
+                  margin='normal'
+                  required
+                  fullWidth
+                  name='password'
+                  label='Password'
+                  type='password'
+                  id='password'
+                  autoComplete='current-password'
+                />
+                <FormControlLabel
+                  control={<Checkbox value='remember' color='primary' />}
+                  label='Remember me'
+                />
+                <Button
+                  type='submit'
+                  fullWidth
+                  variant='contained'
+                  color='primary'
+                  className={Style.submitButton}
                 >
-                  <TextField
-                    margin='normal'
-                    required
-                    fullWidth
-                    id='email'
-                    label='Email Address'
-                    name='email'
-                    autoComplete='email'
-                    autoFocus
-                  />
-                  <TextField
-                    margin='normal'
-                    required
-                    fullWidth
-                    name='password'
-                    label='Password'
-                    type='password'
-                    id='password'
-                    autoComplete='current-password'
-                  />
-                  <FormControlLabel
-                    control={<Checkbox value='remember' color='primary' />}
-                    label='Remember me'
-                  />
-                  <Button
-                    type='submit'
-                    fullWidth
-                    variant='contained'
-                    sx={{ mt: 3, mb: 2 }}
-                  >
-                    Sign In
-                  </Button>
-                  <Grid container>
-                    <Grid item xs>
-                      <Link href='#' variant='body2'>
-                        Forgot password?
-                      </Link>
-                    </Grid>
-                    <Grid item>
-                      <Link href='#' variant='body2'>
-                        {"Don't have an account? Sign Up"}
-                      </Link>
-                    </Grid>
+                  Sign In
+                </Button>
+                <Grid container>
+                  <Grid item xs>
+                    <Link href='#' variant='body2'>
+                      Forgot password?
+                    </Link>
                   </Grid>
-                </Box>
+                  <Grid item>
+                    <Link href='#' variant='body2'>
+                      {"Don't have an account? Sign Up"}
+                    </Link>
+                  </Grid>
+                </Grid>
               </Box>
-              <Copyright sx={{ mt: 8, mb: 4 }} />
-            </Container>
-          </ThemeProvider>
+            </Box>
+            <Copyright className={Style.copyrightMargin} />
+          </Container>
         </Box>
       </Fade>
     </Modal>
