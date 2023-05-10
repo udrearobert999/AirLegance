0f64544d0256ec78fb82725abc9b6e5178e3c179
Andi.Vaduva <andiallex@gmail.com>
2023-05-11 01:07:02 +0300

    - Created a contact form for the Contact page

--- Frontend/src/Pages/Contact/Contact.jsx
+++ Frontend/src/Pages/Contact/Contact.jsx
@@ -1,17 +1,44 @@
 import * as React from 'react';
 import Autocomplete from '@mui/material/Autocomplete';
-import { Typography } from '@material-ui/core'
-
-
-
+import Typography from '@mui/material/Typography';
 import Style from "./Contact.module.css"
+import { Card } from '@material-ui/core';
+import { CardContent } from '@mui/material';
+import { Grid } from '@mui/material';
+import { TextField } from '@mui/material';
+import { Button } from '@mui/material';
 
 export default function Contact() {
   return (
     <div className="Contact">
       <Typography gutterBottom variant="h3" allingn="center">
-        Contact-Us
-
+        Contact Us
+        <Card style={{maxWidth: 450, margin:"0 auto", padding:"20px 5px"}}>
+          <CardContent>
+            <form>
+            <Grid container spacing={1}>
+              <Grid xs={12} sm={6} item>
+                <TextField label="First Name" placeholder="Enter First Name" fullWidth required />
+              </Grid>
+              <Grid xs={12} sm={6} item>
+                <TextField label="Last Name" placeholder="Enter Last Name" fullWidth required />
+              </Grid>
+              <Grid xs={12} item>
+                <TextField type='email' label="Email" placeholder="Enter email" fullWidth required />
+              </Grid>
+              <Grid xs={12} item>
+                <TextField type='number' label="Phone" placeholder="Enter phone number" fullWidth required />
+              </Grid>
+              <Grid xs={12} item>
+                <TextField label="Message" multiline rows={4} placeholder="Type your message here" fullWidth required />
+              </Grid>
+              <Grid xs={12} item>
+                <Button type="submit" variant="contained" color="primary" fullWidth>Submit</Button>
+              </Grid>
+            </Grid>
+            </form>
+          </CardContent>
+        </Card>
       </Typography>
     </div>
   );
