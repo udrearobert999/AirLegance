d3451c1bad96c5204b1ccb58c054814b62262b03
Vasilescu Alexandru <vasilescu.alexandru.a3m@student.ucv.ro>
2023-05-28 16:10:16 +0300

    LabSE-add id login button

--- Frontend/src/Components/LoginModal/LoginModal.jsx
+++ Frontend/src/Components/LoginModal/LoginModal.jsx
@@ -57,6 +57,7 @@ const LoginModal = ({ open, handleClose }) => {
 
   return (
     <Modal
+      id="login-modal"
       aria-labelledby='transition-modal-title'
       aria-describedby='transition-modal-description'
       open={open}
@@ -114,6 +115,7 @@ const LoginModal = ({ open, handleClose }) => {
                   label='Remember me'
                 />
                 <Button
+                  id="login-button"
                   type='submit'
                   fullWidth
                   variant='contained'
