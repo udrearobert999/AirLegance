30301d2d473ceab98e1eb0d3167f5d861a7f1579
Robert Udrea <udrearobert999@gmail.com>
2023-05-30 01:55:28 +0300

    no message

--- Frontend/src/Pages/Home/Home.jsx
+++ Frontend/src/Pages/Home/Home.jsx
@@ -1,4 +1,6 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
+
+import useAxiosWithErrorRedirect from 'Hooks/useAxiosWithErrorRedirect';
 
 import Style from './Home.module.css';
 import Box from '@mui/material/Box';
@@ -13,36 +15,37 @@ import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { DatePicker } from '@mui/x-date-pickers';
 
-const locations = [
-  { label: 'Craiova,Romania' },
-  { label: 'Bucharest,Romania' },
-  { label: 'Iasi,Romania' },
-  { label: 'Cluj,Romania' },
-  { label: 'Brasov,Romania' },
-  { label: 'Rome,Italy' },
-  { label: 'Milano,Italy' },
-  { label: 'Paris,France' },
-  { label: 'Berlin,Germany' },
-  { label: 'Madrid,Spain' },
-  { label: 'Barcelona,Spain' },
-  { label: 'Budapesta,Hungary' },
-];
-
 const Home = () => {
   const departureDateRef = useRef(null);
   const returnDateRef = useRef(null);
   const originRef = useRef(null);
   const destinationRef = useRef(null);
 
+  const axios = useAxiosWithErrorRedirect();
+
+  const [locations, setLocations] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('/locations');
+        setLocations(response.data.data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   const handleSearch = (event) => {
     event.preventDefault();
 
-    const formData = new FormData(event.currentTarget);
     const searchData = {
-      origin: formData.get('origin'),
-      destination: formData.get('destination'),
-      departureDate: formData.get('departureDate'),
-      returnDate: formData.get('returnDate'),
+      origin: originRef.current?.value,
+      destination: destinationRef.current?.value,
+      departureDate: departureDateRef.current?.value,
+      returnDate: returnDateRef.current?.value,
     };
     console.log(searchData);
   };
@@ -84,17 +87,24 @@ const Home = () => {
                   ref={originRef}
                   sx={{ width: '15rem' }}
                   id='origin'
-                  options={locations}
+                  options={locations.map((location) => ({
+                    label: `${location.city} (${location.country})`,
+                    value: location.id,
+                  }))}
                   getOptionLabel={(option) => option.label}
                   renderInput={(params) => (
                     <TextField {...params} label='Origin' fullWidth />
                   )}
                 />
+
                 <Autocomplete
-                  ref={destinationRef}
+                  inputRef={destinationRef}
                   sx={{ width: '15rem' }}
                   id='destination'
-                  options={locations}
+                  options={locations.map((location) => ({
+                    label: `${location.city} (${location.country})`,
+                    value: location.id,
+                  }))}
                   getOptionLabel={(option) => option.label}
                   renderInput={(params) => (
                     <TextField {...params} label='Destination' fullWidth />
@@ -103,13 +113,13 @@ const Home = () => {
               </Box>
               <Box sx={{ display: 'flex', gap: 0.5, flexWrap: 'wrap' }}>
                 <DatePicker
-                  ref={departureDateRef}
+                  inputRef={departureDateRef}
                   sx={{ width: '15rem' }}
                   label='Departure date'
                   renderInput={(params) => <TextField {...params} />}
                 />
                 <DatePicker
-                  ref={returnDateRef}
+                  inputRef={returnDateRef}
                   sx={{ width: '15rem' }}
                   label='Return date'
                   renderInput={(params) => <TextField {...params} />}
